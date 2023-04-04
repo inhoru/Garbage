56c07beee3be76d46c0f81ea8d7ab813022a767d
inhoru <inhoru126@gmail.com>
2023-04-04 17:47:39 +0900

    JOIN

--- BS.sql
+++ BS.sql
@@ -444,14 +444,14 @@ FROM DUAL;
 -- TRIM(문자열||컬럼)
 --              왼쪽      오른쪽     전부
 -- TRIM(LEADING||TRAILING||BOTH '제거할문자' FROM 문자열||컬럼명)
--- 삭제할 문자열은 한글자만가능!
+    -- 삭제할 문자열은 한글자만가능!
 
 SELECT '                    월요일                 ',TRIM( '                    월요일                 ')
 ,'ZZZZZZ마징가ZZZZZZ',TRIM('Z' FROM 'ZZZZZZ마징가ZZZZZZ'),
 TRIM(LEADING 'Z' FROM 'ZZZZZZ마징가ZZZZZZ'),
 TRIM(TRAILING 'Z' FROM 'ZZZZZZ마징가ZZZZZZ'),
 TRIM(BOTH 'Z' FROM 'ZZZZZZ마징가ZZZZZZ')
-FROM EMPLOYEE;
+FROM DUAL;
 
 -- SUBSTR : 문자열을 잘라내는 기능 * JAVA SUBSTRING메소드와 동일
 -- SUBSTR(문자열||컬럼명,시작인덱스번호,[,길이])
@@ -881,8 +881,352 @@ SELECT DECODE(SUBSTR(EMP_NO,8,1),'1','남','3','남','2','여','4','여') AS 성별, A
 FROM EMPLOYEE
 GROUP BY DECODE(SUBSTR(EMP_NO,8,1),'1','남','3','남','2','여','4','여');
 
+-- 각 그룹별 집계와 총 집계를 한번에 출력해주는 함수
+-- ROLLUP(), CUBE()
+-- GROUP BY ROLLUP(컬럼명)
+-- GROUP BY CUBE(컬럼명)
+--부서별 급여합계와 총합계를 조회하는 구문
+SELECT SUM(SALARY)
+FROM EMPLOYEE;
+SELECT DEPT_CODE, SUM(SALARY)
+FROM EMPLOYEE
+WHERE DEPT_CODE IS NOT NULL
+GROUP BY DEPT_CODE;
+
+SELECT DEPT_CODE, SUM(SALARY)
+FROM EMPLOYEE
+WHERE DEPT_CODE IS NOT NULL
+GROUP BY ROLLUP(DEPT_CODE);
+
+SELECT DEPT_CODE, SUM(SALARY)
+FROM EMPLOYEE
+WHERE DEPT_CODE IS NOT NULL
+GROUP BY CUBE(DEPT_CODE);
+
+SELECT JOB_CODE, SUM(SALARY)
+FROM EMPLOYEE
+--GROUP BY ROLLUP(JOB_CODE);
+GROUP BY CUBE(JOB_CODE);
+
+-- ROLLUP, CUBE 함수의 인수로 한개이상의 컬럼을 넣을 수 있다.
+-- ROLLUP : 두개이상의 인수를 전달했을때 두개 컬럼의 집계, 첫번째 인수컬럼의 소계, 전체 총계
+-- CUBE : 두개이상 인수를 전달했을때 두개 컬럼의 집계, 첫번째 인수컬럼의 소계, 두번째 인수컬럼의 소개 , 전체 총계
+
+SELECT DEPT_CODE, JOB_CODE, SUM(SALARY)
+FROM EMPLOYEE
+WHERE DEPT_CODE IS NOT NULL 
+GROUP BY ROLLUP(DEPT_CODE, JOB_CODE);
+
+SELECT DEPT_CODE, JOB_CODE, SUM(SALARY)
+FROM EMPLOYEE
+WHERE DEPT_CODE IS NOT NULL 
+GROUP BY CUBE(DEPT_CODE, JOB_CODE);
 
 
+-- 부서별 , 직책별, 총사원을 한번에 조회하기
+SELECT DEPT_CODE,JOB_CODE, COUNT(*)
+FROM EMPLOYEE
+WHERE DEPT_CODE IS NOT NULL
+GROUP BY CUBE(DEPT_CODE, JOB_CODE);
+
+-- GROUPING함수를 이용하면 집계한 결과에 대한 분기처리르 할 수 있다.
+-- ROLLUP, CUBE로 집계된 ROW에 대한 분기처리
+-- GROUPING 함수를 실행하면, ROLLUP, CUBE로 집계된 ROW1을 반환 아니면 0을 반환
+
+SELECT COUNT(*),DEPT_CODE, JOB_CODE,
+CASE
+WHEN GROUPING(DEPT_CODE)=0 AND GROUPING(JOB_CODE)=1 THEN '부서별인원'
+WHEN GROUPING(DEPT_CODE)=1 AND GROUPING(JOB_CODE)=0 THEN '직책별인원'
+WHEN GROUPING(DEPT_CODE)=0 AND GROUPING(JOB_CODE)=0 THEN '부서_직책인원'
+WHEN GROUPING(DEPT_CODE)=1 AND GROUPING(JOB_CODE)=1 THEN '총인원'
+END AS 결과
+FROM EMPLOYEE
+WHERE DEPT_CODE IS NOT NULL
+GROUP BY CUBE(DEPT_CODE, JOB_CODE);
+
+-- 테이블에서 조회한 데이터 정렬하기
+-- ORDER BY 구문을 사용함.
+-- SELECT 컬럼명.....
+-- FROM 테이블명
+-- [WHERE 조건식]
+-- [GROUP BY 컬럼명]
+-- [HAVING 조건식]
+-- 정렬방법은 생략가능 디포틀값으로 나옴 디폴트값 = 오름차순
+-- [ORDER BY 컬럼명 정렬방식(DESC(내림),ASC(오름, DEFAULT)]
+
+-- 이름을 기준으로 정렬하기
+--오름차순
+SELECT *
+FROM EMPLOYEE
+ORDER BY EMP_NAME;
+
+--내림차순
+SELECT *
+FROM EMPLOYEE
+ORDER BY EMP_NAME DESC;
+
+-- 월급이 높은사람부터 낮은사람으로 정렬하기
+-- 이름 , 급여, 보너스
+SELECT EMP_NAME, SALARY, NVL(BONUS,0)
+FROM EMPLOYEE
+ORDER BY SALARY DESC;
+
+-- 부서코드를 기준으로 오름차순정렬하고 값이 같으면 월급이 내림차순으로 정렬하기
+SELECT *
+FROM EMPLOYEE
+ORDER BY DEPT_CODE ASC, SALARY DESC, EMP_NAME ASC;
+
+-- 정렬햇을때 NULL값에 대한 처리
+-- BONUS를 많이 받는 사원부터 출력하기
+SELECT *
+FROM EMPLOYEE
+--ORDER BY BONUS  DESC; -- NULL인 값을 먼저 출력한다.
+--ORDER BY BONUS  ASC; -- NULL인 값을 나중에 출력한다.
+-- 옵션을 설정해서 NULL값출력위치를 변경할 수 있다.
+--ORDER BY BONUS ASC NULLS FIRST;
+ORDER BY BONUS ASC NULLS LAST;
+
+
+-- ORDER BY 절에서는 별칭을 사용할 수 있음
+SELECT EMP_NAME, SALARY AS 월급, BONUS
+FROM EMPLOYEE
+-- WHERE 에는 쓰지못한다'월급'  FROM을먼저 그다음 웨월 그다음에 별칭을 부여하기때문에 불가능하다.
+ORDER BY 월급;
+
+
+-- SELECT문을 이용해서 데이터를 조회하면 RESULT SET 이 출력되는데
+-- RESULT SET 에 출력되는 컬럼에는 자동으로 INDEX번호가 1부터 부여가 된다.
+SELECT *
+FROM EMPLOYEE
+-- SELECT 는 출력시 컬럼명에 순서대로 인덱스부여한다 그인덱스번호를 사용할수있다.
+ORDER BY 2;
+
+
+--  집합연산자
+-- 여러개의 SELECT문을 한개의 결과(RESULT SET)으로 출력해주는 것
+-- 첫번째 SELECT문의 컬럼수와 이후 SELECT문의 컬럼수가 같아야한다.
+-- 각 컬럼별 데이터 타입도 동일해아한다.
+
+-- UNION : 두개이상의 SELECT문을 합치는 연산자
+-- SELECT 문 UNION SELECT문
+
+-- UNION : 중복값이 있을때 하나만 출력된다.
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D5'
+UNION
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE SALARY>=3000000;
+
+-- UNION ALL :  중복값이있다면 중복값 포함시킨다
+-- MINUIS : 중복값 포함 하지않는다.,
+-- INTERSECT :  중복값만 가져오다.
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D5'
+INTERSECT
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE SALARY>=3000000;
+
+
+
+SELECT EMP_ID, EMP_NAME, SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE='D5'
+UNION
+-- 두 SELECT문의 컬럼의 수가 다르면 안된다.
+--SELECT EMP_ID,EMP_NAME,SALARY,BONUS
+--FROM EMPLOYEE
+--WHERE SALARY>3000000;
+
+-- 두개의 SELECT문의 컬럼의 타입도 맞춰야 한다.
+SELECT EMP_ID,EMP_NAME,EMP_NO
+FROM EMPLOYEE
+WHERE SALARY>3000000;
+
+-- 다른 테이블에 있는 데이터를 합치기
+-- 맞는 컬럼이없을경우 고정값(리터럴값)을 정해서 넣을수도있다.
+SELECT EMP_ID, EMP_NAME, SALARY
+FROM EMPLOYEE
+UNION
+SELECT DEPT_ID, DEPT_TITLE , 10
+FROM DEPARTMENT;
+
+SELECT EMP_ID,EMP_NAME
+FROM EMPLOYEE
+UNION
+SELECT DEPT_ID, DEPT_TITLE
+FROM DEPARTMENT
+UNION
+SELECT JOB_CODE, JOB_NAME
+FROM JOB
+MINUS
+SELECT EMP_ID, EMP_NAME
+FROM EMPLOYEE
+WHERE DEPT_CODE IN ('D5','D6','D7');
+
+-- GROUPING SET
+-- 여러 GROUP BY 절이 있는 구문을 하나로 작성하게 해주는 기능
+-- 부서, 직책, 매니저별 급여평균
+SELECT DEPT_CODE, JOB_CODE, MANAGER_ID , AVG(SALARY)
+FROM EMPLOYEE
+GROUP BY DEPT_CODE,JOB_CODE, MANAGER_ID;
+-- 부서, 직책별, 급여평균
+SELECT DEPT_CODE, JOB_CODE , AVG(SALARY)
+FROM EMPLOYEE
+GROUP BY DEPT_CODE,JOB_CODE;
+-- 부서, 매니저별 급여평균
+SELECT DEPT_CODE,  MANAGER_ID , AVG(SALARY)
+FROM EMPLOYEE
+GROUP BY DEPT_CODE, MANAGER_ID;
+
+-- GROUPING SET 을 이용해서 묶어서 사용해줄수있다.
+SELECT DEPT_CODE, JOB_CODE, MANAGER_ID, AVG(SALARY)
+FROM EMPLOYEE
+GROUP BY GROUPING SETS((DEPT_CODE,JOB_CODE,MANAGER_ID),(DEPT_CODE,JOB_CODE),(DEPT_CODE, MANAGER_ID));
+
+
+ --  JOIN에 대해 알아보자
+ -- 두개이상의 테이블을 특정컬럼을 기준으로 연결해준, 기능
+ -- JOIN은 두 종류가 있음
+ -- 1. INNER JOIN : 기준되는 값이 일치하는 ROW만 가져오는 JOIN
+ -- 2. OUTER JOIN :  기준이되는 값이 일치하지 않은 ROW도 가져오는 JOIN * 기준이 필요
+ 
+ -- JOIN을 작성하는 방법 2가지
+ -- 1. 오라클 조인방식 :  , 와 WHERE로 작성
+ -- 2. ANSI 표준 조인방식 :  JOIN, ON | | USING 예약어를 사용해서 작성
+ 
+ -- EMPLOYEE테이블과 DEPARTMENT테이블 JOIN하기
+ SELECT* FROM EMPLOYEE;
+ SELECT* FROM DEPARTMENT;
+ 
+ -- 오라클방식으로 JOIN하기
+ SELECT *
+ FROM EMPLOYEE;
+ 
+ SELECT*
+ FROM EMPLOYEE, DEPARTMENT
+ WHERE EMPLOYEE.DEPT_CODE = DEPARTMENT.DEPT_ID;
+ 
+ -- ANSI 표준으로 JOIN하기
+ SELECT *
+ FROM EMPLOYEE 
+ JOIN DEPARTMENT ON EMPLOYEE.DEPT_CODE = DEPARTMENT.DEPT_ID;
+ 
+-- 사원에 대해 사원명, 이메일, 전화번호, 부서명을 조회하기
+SELECT EMP_NAME, EMAIL, PHONE,DEPT_TITLE
+FROM EMPLOYEE 
+JOIN DEPARTMENT ON DEPT_CODE=DEPT_ID; 
+
+ -- JOIN 문에서도 WHERE절 사용하기
+ -- 부서가 총무부인 사원  사원명, 월급, 보너스, 부서명 조회하기
+ SELECT EMP_NAME, SALARY,BONUS, DEPT_TITLE
+ FROM EMPLOYEE
+ JOIN DEPARTMENT ON DEPT_CODE=DEPT_ID
+ WHERE DEPT_TITLE = '총무부';
+ 
+ -- JOIN 문에서 GROUP BY 절 사용하기
+ -- 부서별 평균급여를 출력하기 부서명, 평균급여
+ SELECT DEPT_TITLE,AVG(SALARY)
+ FROM EMPLOYEE
+ JOIN DEPARTMENT ON DEPT_CODE = DEPT_ID
+ GROUP BY DEPT_TITLE
+HAVING AVG(SALARY)>=3000000;
+
+-- JOIN할때 기준이되는 컬럼명이 중복된다면 반드시 별칭을 작성해야한다.
+-- 사원명, 급여, 보너스, 직책명을 조회하기
+-- 다른 테이블이지만 컬럼명이 같앗을경우 오류가뜬다. 
+-- ambiguously 오류가떳을때 테이블앞에 별칭을 부여하던 명칭을 부여해야한다.
+SELECT * --EMP_NAME ,SALARY, BONUS, JOB_NAME
+FROM EMPLOYEE E
+    JOIN JOB J ON E.JOB_CODE=J.JOB_CODE;
+
+-- 중복되는 컬럼명으로 조인할 때는 USING을 이용할 수 있다.
+-- 중복되는 코드가 하나만 출력이된다.
+-- 하나만 출력되기때문에 별칭부여해서 구분할필요가없다.
+-- USING 을사용할때 식별자(별칭)를 쓰지않는다.
+SELECT * --EMP_NAME, SALARY, BONUS, JOB_NAME
+FROM EMPLOYEE
+    JOIN JOB  USING(JOB_CODE)
+WHERE JOB_CODE='J3'; 
+SELECT * FROM JOB;
+
+-- 직책이 과장인 사원의 이름, 직책명, 직책코드, 월급을 조회하세요.
+SELECT  EMP_NAME, JOB_NAME,JOB_CODE, SALARY
+FROM EMPLOYEE
+JOIN JOB USING(JOB_CODE)
+WHERE JOB_NAME = '과장';
+
+--- JOIN,은 NULL값을 무시한다.
+SELECT COUNT(*)
+FROM EMPLOYEE
+JOIN DEPARTMENT ON DEPT_CODE=DEPT_ID;
+
+SELECT * FROM EMPLOYEE;
+
+-- OUTER JOIN사용하기
+-- 컬럼에 대해 동일비교를 했을때 없는 ROW를 출력해주는 JOIN
+-- 기준이 되는 테이블(모든데이터를 출력함)을 설정해줘야한다.
+-- LEFT OUTER JOIN : JOIN을 기준으로 왼쪽에 있는 테이블을 기준으로 설정
+-- RIGHT OUTER JOIN : JOIN을 기준으로 오른쪽에 있는 테이블을 기준으로 설정
+-- 일치되는 ROW가 없는 경우 모든 컬럼을 NULL로 표시한다.
+-- OUTER 은 생략해도 상관없음
+SELECT *
+FROM EMPLOYEE LEFT  JOIN DEPARTMENT ON DEPT_CODE=DEPT_ID;
+
+SELECT *
+FROM EMPLOYEE RIGHT OUTER JOIN DEPARTMENT ON DEPT_CODE=DEPT_ID;
+
+-- CROSS JOIN : 모든 ROW를 연결해주는 JOIN
+-- 
+SELECT EMP_NAME, DEPT_TITLE
+FROM EMPLOYEE CROSS JOIN DEPARTMENT
+ORDER BY 1; 
+
+-- SELF JOIN : 한개의 테이블에 다른 컬럼의 값을 가지고 있는 컬럼이 있는 경우 그두개 컬럼을 이용해서 JOIN
+SELECT * FROM EMPLOYEE;
+-- MANAGER가 있는 사원의 이름, 매니저 아이디, 매니저 사원번호, 매니저 이름 조회
+SELECT E.EMP_NAME,E.MANAGER_ID,M.EMP_ID,M.EMP_NAME
+FROM EMPLOYEE E
+JOIN EMPLOYEE M ON E.MANAGER_ID=M.EMP_ID;
+
+-- 사원이름, 매니저아이디, 매니저사원번호, 매니저 이름 조회
+-- 매니저가 없으면 없을 출력하기
+SELECT E.EMP_NAME,NVL(E.MANAGER_ID,'없음'),NVL(M.EMP_ID,'없음') ,NVL(M.EMP_NAME,'없음')
+FROM  EMPLOYEE E
+         LEFT   JOIN EMPLOYEE M ON E.MANAGER_ID=M.EMP_ID;
+
+SELECT * FROM EMPLOYEE;
+
+
+-- 동등조인 동등비교를 해서 처리함. ON 컬럼명=컬럼명
+-- 비동등조인에 대해 알아보자
+-- 연결할 테이블이 범위값을 가져야한다.
+SELECT * FROM SAL_GRADE;
+SELECT * 
+FROM EMPLOYEE
+        JOIN SAL_GRADE ON SALARY BETWEEN MIN_SAL AND  MAX_SAL;
+-- 회원등급 포이튼재로, 상품등급(상태) , 댓글 수에 따른 회원등급
+
+-- 다중조인을 할 수 있다.
+-- 3개이상의 테이블을 연결해서 사용하기
+--사원의 사원명, 직책명, 부서명을 조회하기
+SELECT EMP_NAME,JOB_NAME,DEPT_TITLE
+FROM EMPLOYEE
+        JOIN DEPARTMENT ON DEPT_CODE=DEPT_ID
+        JOIN JOB  USING(JOB_CODE);
+        
+-- 사원의 사원명, 부서명, 직챙명, 근무지역(LOCALNAME)조회하기
+-- 부서명이 없으면 대기, 근무지역이 없으면 대기발령을 출력
+SELECT EMP_NAME,NVL(DEPT_TITLE,'대기'),JOB_NAME,NVL(LOCAL_NAME,'대기발령')
+FROM EMPLOYEE
+        LEFT JOIN DEPARTMENT ON DEPT_CODE=DEPT_ID
+        LEFT JOIN LOCATION ON LOCATION_ID=LOCAL_CODE
+        JOIN JOB USING(JOB_CODE);
+      
+        
 
 
 
